f7df8dc5bcc85f9aa8488bf245db2ec2062a43bc
grishatop1 <grigorije05@gmail.com>
2022-12-14 22:01:30 +0100

    reveal elements

--- src/script.js
+++ src/script.js
@@ -112,3 +112,19 @@ const getOffset = (el) => {
         height: rect.height
     };
 }
+
+const scrollReveal = () => {
+	var revealPoint = 150;
+	var revealElement = document.querySelectorAll(".reveal");
+	for (var i = 0; i < revealElement.length; i++) {
+		var windowHeight = window.innerHeight;
+		var revealTop = revealElement[i].getBoundingClientRect().top;
+		if (revealTop < windowHeight - revealPoint) {
+			revealElement[i].classList.add("active");
+		} else {
+			revealElement[i].classList.remove("active");
+		}
+	}
+}
+
+window.addEventListener("scroll", scrollReveal);
